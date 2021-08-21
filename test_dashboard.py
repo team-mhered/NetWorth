e1bebff9bfd2d903c5e01330146d093a97ed9dc7
Manuel Heredia <manolo.heredia@gmail.com>
2021-08-22 01:27:25 +0200

    Basic HUs balance #15, piechart #16 and close bug #25
    * Define plot_piechart(), an auxiliary function that uses matplotlib to
    plot a simple piechart, from a dictionary of {labels:values in
    percentage} with the slices ordered counter-clockwise. Address Issue #15.
    
    * Define get_portfolio_piechart(), a method for Portfolio that returns
    a dictionary ready to use as input for plot_piechart() to plot a
    piechart of the Portfolio assets grouped by subcategories on a given
    date in the Portfolio currency. Address Issue #15.
    
    * Redesign display() methods of Portfolio, Item and HistoryPoint so
    that HistoryPoint.display() no longer needs to access the currency
    of the parent Portfolio. Also reformat the output to show a more
    condensed HISTORY table.
    
    * Moved get_portfolio_currency() method from HistoryPoint to the
    Portfolio class. Exception no longer needed hence removed from
    the function. Close the ::bug:: described in Issue #25
    (catching orphan Items).
    
    * Move ./tests/test_balance.py to ./test_dashboard.py and modify to
    include tests of both balance and piechart functions. Close #15, #16.
    
    Close Issues #15 #16 #25
    
                                             50char->|             72char->|

--- test_dashboard.py
+++ test_dashboard.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-""" Crude tests for get_portfolio_balance()"""
+""" Crude tests for get_portfolio_balance() and get_portfolio_piechart()"""
 
-from utils import Portfolio  # , Item, HistoryPoint, generate_unique_id
+from utils import Portfolio, plot_piechart
 import logging
 from datetime import date
 
@@ -20,35 +20,35 @@ def main():
 
     logging.info('\n-------RUN STARTS-------')
     logging.info('Create a Portfolio')
-    portfolio1 = Portfolio(name="My First Portfolio", currency="USD",
-                           description="Test portfolio")
-    logging.debug("Print 'portfolio1' with empty list of items:\n %s",
-                  portfolio1.display())
+    my_portfolio = Portfolio(name="My First Portfolio", currency="EUR",
+                             description="Test portfolio")
+
+    logging.debug("Print 'my_portfolio' with empty list of items:\n %s",
+                  my_portfolio.display())
 
     logging.info("Add 'fund1' Item")
-
-    fund1 = portfolio1.add_item(category='asset', subcategory='fund',
-                                currency='EUR', name='Fondo NARANJA 50/40',
-                                description='Investment fund in ING Direct')
+    fund1 = my_portfolio.add_item(category='asset', subcategory='fund',
+                                  currency='EUR', name='Fondo NARANJA 50/40',
+                                  description='Investment fund in ING Direct')
 
     logging.debug("Print 'fund1' without history:\n\n %s", fund1.display())
 
     logging.info("Update History of 'fund1' Item")
-
     fund1.update_history(when=date(2021, 8, 16),
                          units_owned=1, cost_of_purchase=100000,
                          value_of_asset=107963.89)
+
     logging.debug("Print 'fund1' with 1 history point:\n\n %s",
                   fund1.display())
 
-    logging.debug("Print 'portfolio1' with item 'fund1':\n %s",
-                  portfolio1.display())
+    logging.debug("Print 'my_portfolio' with item 'fund1':\n %s",
+                  my_portfolio.display())
 
     logging.info("Add 'crypto1' Item")
 
-    crypto1 = portfolio1.add_item(category='asset', subcategory='currency',
-                                  currency='BTC', name='Bitcoin',
-                                  description='Bitcoin in Revolut')
+    crypto1 = my_portfolio.add_item(category='asset', subcategory='currency',
+                                    currency='BTC', name='Bitcoin',
+                                    description='Bitcoin in Revolut')
 
     logging.debug("Print 'crypto1' without history:\n\n %s",
                   crypto1.display())
@@ -56,40 +56,53 @@ def main():
     logging.info("Update History of 'crypto1' Item")
 
     crypto1.update_history(when=date(2021, 5, 12),
-                           units_owned=1.1, cost_of_purchase=50083.18,
-                           value_of_asset=30000)
+                           units_owned=1, cost_of_purchase=50083.18,
+                           value_of_asset=1.1)
 
     crypto1.update_history(when=date(2021, 8, 16),
-                           units_owned=1.4, cost_of_purchase=59407.83,
-                           value_of_asset=45000)
+                           units_owned=1, cost_of_purchase=59407.83,
+                           value_of_asset=1.4)
 
     crypto1.update_history(when=date(2021, 8, 21),
-                           units_owned=1.4, cost_of_purchase=59407.83,
-                           value_of_asset=60000)
+                           units_owned=1, cost_of_purchase=59407.83,
+                           value_of_asset=1.4)
 
     crypto1.update_history(when=date(2021, 6, 1),
-                           units_owned=1.4, cost_of_purchase=59407.83,
-                           value_of_asset=35000)
+                           units_owned=1, cost_of_purchase=59407.83,
+                           value_of_asset=1.4)
 
     logging.debug("Print 'crypto1' with 4 history points:\n\n %s",
                   crypto1.display())
 
-    logging.info("Print 'portfolio1' with 2 items with history:\n %s",
-                 portfolio1.display())
+    logging.info("Print 'my_portfolio' with 2 items with history:\n %s",
+                 my_portfolio.display())
 
+    """
     logging.info('Testing get_item_balance...\n')
     for i in [14, 15, 16, 17, 19, 20, 21]:
         dat = date(2021, 8, i)
         balance = crypto1.get_item_balance(currency='EUR', given_date=dat)
         logging.debug("Balance of 'crypto1' in EUR on %s:\n %s",
                       dat.isoformat(), str(balance))
+    """
 
-    logging.info('Testing get_portfolio_balance...\n')
-    for i in [14, 15, 16, 17, 19, 20, 21]:
+    logging.info('Testing dashboard: balance & piechart...\n')
+    for i in [14, 17, 19, 21]:
         dat = date(2021, 8, i)
-        balance = portfolio1.get_portfolio_balance(given_date=dat)
-        logging.debug("Balance of 'portfolio1' in EUR on %s:\n %s",
-                      dat.isoformat(), str(balance))
+        balance = my_portfolio.get_portfolio_balance(given_date=dat)
+        logging.info("Balance of 'my_portfolio' in %s on %s:\n %s",
+                     my_portfolio.currency, dat.isoformat(), str(balance))
+
+        piechart = my_portfolio.get_portfolio_piechart(given_date=dat)
+        logging.info("Piechart of 'my_portfolio' in %s on %s:\n %s",
+                     my_portfolio.currency, dat.isoformat(), str(piechart))
+        # plot_piechart(piechart)
+
+    """
+    dict = {'Frogs': 15, 'Hogs': 30, 'Dogs': 45, 'Logs': 10}
+    plot_piechart(dict)
+    a = input("Press Enter\n")
+    """
 
     logging.info('Success')
     logging.info('\n--------RUN ENDS--------\n')
