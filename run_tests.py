913741c483fdba6b3c0b889eaa4b34fb3948e411
Manuel Heredia <manolo.heredia@gmail.com>
2021-09-01 01:18:03 +0200

    50char->|             72char->|
    Set up unittest for better testing
    
    Rewrite tests and reorganize project folder to use unittest:
    
    * Replace crude tests in 'test_dashboard.py' and 'test_create.py' with
      proper tests with unittest in './tests/test_utils.py'
    
    * Replace crude tests in 'test_exchange.py' with proper tests with
      unittest in './tests/test_exchange.py'
    
    * Create './src' folder and move 'utils.py' to './src/utils.py'
    
    * Create empty '__init__.py' in '.', './tests/' and './src/' folders to
      make packages and allow imports from test modules to work
    
    * Create 'run_tests.py' to execute the full test suite.
      Usage: $ python3 -m run_tests
    
    Resulting tree:
        .
        ├── agil.yaml
        ├── assets
        ├── docs
    NEW ├── __init__.py
        ├── LICENSE
        ├── personas.md
        ├── rcpylint
        ├── README.md
    NEW ├── run_tests.py
    NEW ├── src
    NEW │   ├── __init__.py
    MV  │   └── utils.py
        ├── tasks.py
        └── tests
    NEW     ├── __init__.py
            ├── README.md
    NEW     ├── test_exchange.py
    NEW     └── test_utils.py

---
+++ run_tests.py
@@ -0,0 +1,25 @@
+# tests/runner.py
+
+import unittest
+
+# import your test modules
+import tests.test_utils as test_utils
+import tests.test_exchange as test_exchange
+# import tests.test_others as test_others
+
+
+# initialize the test suite
+loader = unittest.TestLoader()
+suite = unittest.TestSuite()
+
+# run test methods in order
+loader.sortTestMethodsUsing = None
+
+# add tests to the test suite
+suite.addTests(loader.loadTestsFromModule(test_utils))
+suite.addTests(loader.loadTestsFromModule(test_exchange))
+# suite.addTests(loader.loadTestsFromModule(test_others))
+
+# initialize a runner, pass it your suite and run it
+runner = unittest.TextTestRunner(verbosity=3)
+result = runner.run(suite)
