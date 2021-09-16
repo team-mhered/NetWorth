5560112cddc2257ff939434e78a6acf63ff48247
Manuel Heredia <manolo.heredia@gmail.com>
2021-09-17 01:06:10 +0200

    Text UI, read/write, Item.ledger, close #40.
    1) Basic text UI
     * Create src/ui.py:
       * UI_purchase(): simple text-based interactive user interface
         with a menu to define a purchase transaction. Dependencies: PyInquirer.
    
       * UI_menu(): simple text-based interactive user interface with
         a menu to interact with the app. Dependencies: PyInquirer.
         Auxiliary functions:
         * show_portfolio() - calls Portfolio.display()
         * analyze_portfolio() - calls piechart()
         * new_item() - requests user input and calls Portfolio.add_item
         * show_item() - calls Item.display
         * quit_app() - quits
     * Create tests/test_ui.py:
       * Define unittest for UI_purchase()
    
     * In run_tests.py:
       * add test_ui to the suite. It is deactivated because
         prompt throws an error; unittesting of ui.py can be
         called manually with: $ python3 -m tests.test_ui
    
     * Remove src/testinput0.py and src/testinput1.py:
       * obsolete files
    
    2) Basic read/write functions
     * Create src/readwrite.py:
       * auxiliary functions portfolio_from_dict()
         and inverse dict_from_portfolio(). Only partially tested.
    
     * These functions are a step towards implementing read
       and write to file (still pending). Could help also for
       __str__() and __repr__() (also pending)
    
    3) Create Item.ledger and methods
     * In src/utils.py:
       * create attribute Item.ledger to store transactions that
         will allow to reconstruct history
    
       * create method Item. update_ledger()
    
       * in Item.purchase() fix typos in the strings used for
         failed_because. Also call Item.update_ledger method to save
         new purchase transaction
    
    4) Close #40
     * In src/utils.py:
       * Close Issue #40 and make global variables
         into class attributes of the Portfolio Class:
         * supported_categories --> Portfolio.CATEGORIES
         * supported_subcategories --> Portfolio.SUBCATEGORIES
         * supported_currencies -> Portfolio.CURRENCIES
         * supported_crypto --> Portfolio.CRYPTO
    
    Closes Issue #40

--- run_tests.py
+++ run_tests.py
@@ -5,6 +5,7 @@ import unittest
 # import your test modules
 import tests.test_utils as test_utils
 import tests.test_exchange as test_exchange
+import tests.test_ui as test_ui
 # import tests.test_others as test_others
 
 
@@ -18,6 +19,11 @@ loader.sortTestMethodsUsing = None
 # add tests to the test suite
 suite.addTests(loader.loadTestsFromModule(test_utils))
 suite.addTests(loader.loadTestsFromModule(test_exchange))
+
+# UI testing fails when called from the suite (conflict with prompt?).
+# Call manually instead using python3 -m tests.test_ui
+# suite.addTests(loader.loadTestsFromModule(test_ui))
+
 # suite.addTests(loader.loadTestsFromModule(test_others))
 
 # initialize a runner, pass it your suite and run it
